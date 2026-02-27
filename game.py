5d2328d5d821b31319198c9cdfd7bcc7b638a35b
Al-an Baker <alan.baker@learnerme.academy>
2026-02-27 14:34:59 +1300

    Refactored Map Code to use an Object based map system rather than Dictornary

--- game.py
+++ game.py
@@ -56,41 +56,22 @@ color_map = {
     "C": "\033[32mC\033[0m",
     ">": "\033[36m>\033[0m"  
 }
-
-#Here we are generating the map based on the C Array provided
-values = re.findall(r'0x[0-9a-fA-F]+', map1_array)
-
-width = 16
-
-python_grid = []
-for i in range(0, len(values), width):
-    row = values[i:i+width]
-    converted_row = [value_map[val.lower()] for val in row]
-    python_grid.append(converted_row)
-
-maps = {
-    "map1": python_grid,
-    "map2": [
-        [wall]*16,
-        [wall] + [empty]*14 + [wall],
-        [wall] + [empty]*14 + [wall],
-        [wall]*16
-    ]
-}
+def parse_map(array_str, width = 16):
+    #Here we are generating the map based on the C Array provided
+    clean_str = re.sub(r'[\{\}\s]', '', array_str)
+    values = re.findall(r'0x[0-9a-fA-F]+', clean_str)
+    grid = []
+    for i in range(0, len(values), width):
+        row = values[i:i+width]
+        converted_row = [value_map[val.lower()] for val in row]
+        grid.append(converted_row)
+    return grid
 
 
 
 #Game Varibles
 command = ""
-current_map = "map1"
 playerLastMove = True
-
-#Here we set the Game Map to the first Map
-game_map = maps[current_map]
-
-#Add Doors so the Player can move between maps
-maps["map1"][1][15] = door
-maps["map2"][1][0] = door
 
 
 #System Method allows up to clear the console to make the game feel more real
@@ -101,7 +82,7 @@ def clear():
 #Class Definitions
     #The Item Class right now is resposible for Stat Boosts, this is a Class so make scailability easier
 class Item():
-    def __init__(self, token, item, x, y, active):
+    def __init__(self, token, item, x, y, active=True):
         self.token = token
         self.item = item
         self.x = x
@@ -111,7 +92,7 @@ class Item():
 #The Character Class is here to both avoid dulicated code with the Foe and makes it easy to add new Characters to the map
 class Character():
     #Character Vars
-    def __init__(self, token, x, y, NPC, ATK, DEF, HP, current_map):
+    def __init__(self, token, x, y, NPC, ATK, DEF, HP, current_map=None):
 
         #token is how the character looks on the Map
         self.token = token
@@ -130,30 +111,74 @@ class Character():
 
         self.current_map = current_map
 
+class GameMap:
+    def __init__(self, name, grid):
+        self.name = name
+        self.grid = grid
+        self.entities = []
+        self.doors = []
+
+    def add_entity(self, entity):
+        entity.current_map = self
+        self.entities.append(entity)
+
+    def add_door(self, x, y):
+        self.grid[y][x] = door
+        self.doors.append((x, y))
+
+    def draw(self):
+        for y, row in enumerate(self.grid):
+            for x, tile in enumerate(row):
+                entity_here = next((e.token for e in self.entities if e.x == x and e.y == y and e.current_map == self), None)
+                if entity_here:
+                    print(color_map[entity_here], end="")
+                else:
+                    print(color_map[tile], end="")
+            print()
+
+    def enter_map(self, player, x, y):
+        player.x = x
+        player.y = y
+        player.current_map = self
+        global current_map
+        current_map = self
+
+        if player not in self.entities:
+            self.entities.append(player)
+
+map1_grid = parse_map(map1_array)
+map2_grid = [
+    [wall]*16,
+    [wall] + [empty]*14 + [wall],
+    [wall] + [empty]*14 + [wall],
+    [wall]*16
+]
+
+Map1 = GameMap("map1", map1_grid)
+Map2 = GameMap("map2", map2_grid)
+
+Map1.add_door(15,1)
+Map2.add_door(0,1)
+
 #Here we initilise the two current characters with their attrbutes
-Player = Character("@", 7, 13, False, 5, 5, 10, "map1")
+Player = Character("@", 7, 13, False, 5, 5, 5)
+Foe = Character("%", 2, 2, True, 0, 0, 2)
+Chest = Item("C", "Chest", 13, 1)
 
-Foe = Character("%", 2, 2, True, 0, 0, 2, "map1")
+Map1.add_entity(Player)
+Map1.add_entity(Foe)
+Map2.add_entity(Chest)
 
-#And here we initalise the Item Object
-Chest = Item("C", "Chest", 13, 1, False)
-
-Chest.x, Chest.y = 13, 1
-maps["map2"][Chest.y][Chest.x] = Chest.token
+current_map = Map1
 
 #Draw_Game prints the Map onto the console, we do this by interating through both the x axis and y axis 
 def Draw_Game():
-    for row in game_map:
-        for tile in row:
-            print(color_map[tile], end="")
-        print()
-
+    current_map.draw()
 
 #This Deals with PVE combat
 def Combat():
     if Foe.current_map != current_map:
         return
-
     if abs(Player.x-Foe.x)+abs(Player.y-Foe.y)==1 and Foe.HP>0:
         if Player.ATK>Foe.DEF:
             Foe.HP -= 1
@@ -164,77 +189,53 @@ def Combat():
 
 #Try_Move replaces the old delta positions that were used previosly, this new system works better allowing for more expansion
 def Try_Move(character, dx, dy):
-    global game_map, current_map
+    global current_map
     new_x = character.x + dx
     new_y = character.y + dy
-    tile = game_map[new_y][new_x]
 
-
-    if not (0 <= new_y < len(game_map) and 0 <= new_x < len(game_map[0])):
+    if not (0 <= new_y < len(current_map.grid) and 0 <= new_x < len(current_map.grid[0])):
         return False
-    
+
+    tile = current_map.grid[new_y][new_x]
+
     if tile in [wall, water]:
         return False
-    
-    if current_map == "map2" and Player.x == Chest.x and Player.y == Chest.y:
-        game_map[Chest.y][Chest.x] = empty
+
+    if current_map == Map2 and Player.x == Chest.x and Player.y == Chest.y:
+        current_map[Chest.y][Chest.x] = empty  # remove the chest from the map
         Player.ATK += 5
         Player.DEF += 2
         Player.HP += 2
-        print("Player has found a lost item stats are improved!")
-        Chest.x = -1
+        print("Player has found a lost item! Stats are improved!")
+        Chest.x = -1  # deactivate the chest
         Chest.y = -1
+        Chest.active = False
         return True
     
-
     character.x = new_x
     character.y = new_y
 
     if tile == door and character == Player:
-        if current_map == "map1":
-            current_map = "map2"
-            Player.x, Player.y = 1, 1
+        if current_map == Map1:
+            Map2.enter_map(Player, 1, 1)
         else:
-            current_map = "map1"
-            character.x, character.y = 14, 1
-        game_map = maps[current_map]
-        return True
+            Map1.enter_map(Player, 14, 1)
     return True
-
-#Update_Positions Deletes the old characters
-def Update_Positions():
-    for y in range(len(game_map)):
-        for x in range(len(game_map[0])):
-            # Only erase Player and Foe, leave other map objects intact
-            if game_map[y][x] in [Player.token, Foe.token]:
-                game_map[y][x] = empty
-
-    if Foe.HP > 0 and Foe.current_map == current_map:
-        game_map[Foe.y][Foe.x] = Foe.token
-    game_map[Player.y][Player.x] = Player.token
-
-
-#Show the Player's Stats at the top of the screen
-def Print_Stats():
-    print("Defence: "+ str(Player.DEF))
-    print("Attack: "+ str(Player.ATK))
-    print("Health: "+ str(Player.HP))
 
 #This is to move the NPC
 def Foe_Move():
     if Foe.current_map != current_map:
         return
-    aiMove = random.randrange(0, 4)
-    if aiMove == 0: Try_Move(Foe, 0, -1)   # up
-    elif aiMove == 1: Try_Move(Foe, 0, 1)  # down
-    elif aiMove == 2: Try_Move(Foe, -1, 0) # left
-    elif aiMove == 3: Try_Move(Foe, 1, 0)  # right
+    move = random.choice([(0, -1),(0, 1), (-1, 0), (1,0)])
+    Try_Move(Foe, *move)
+#Show the Player's Stats at the top of the screen
+def Print_Stats():
+    print(f"Defence: {Player.DEF} | Attack: {Player.ATK} | Health {Player.HP}")
 
 #Here we have the main Game Loop
 def Game_Loop():
     global playerLastMove
     while True:
-        Update_Positions()
         clear()
         Print_Stats()
         Draw_Game()
@@ -252,6 +253,5 @@ def Game_Loop():
             Try_Move(Player, 1, 0)
         elif command == "q":
             quit()
-
         Foe_Move()
 Game_Loop()
